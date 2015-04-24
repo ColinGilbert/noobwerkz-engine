e678b9be5e44f9b1ac6f3e3d6c62caac682682bc
ColinGilbert <colingilbert86@gmail.com>
2015-04-24 13:46:04 -0600

    Screen rotate bug fixed :)

--- engine/android/gen/R.java.d
+++ engine/android/gen/R.java.d
@@ -3,4 +3,7 @@
 /home/noob/noobgame/engine/android/res/values/strings.xml \
 /home/noob/noobgame/engine/android/res/drawable-hdpi/ic_launcher.png \
 /home/noob/noobgame/engine/android/res/drawable-mdpi/ic_launcher.png \
+/home/noob/noobgame/engine/android/bin/res/drawable-xhdpi/ic_launcher.png \
+/home/noob/noobgame/engine/android/bin/res/drawable-hdpi/ic_launcher.png \
+/home/noob/noobgame/engine/android/bin/res/drawable-mdpi/ic_launcher.png \
 /home/noob/noobgame/engine/android/bin/AndroidManifest.xml \
