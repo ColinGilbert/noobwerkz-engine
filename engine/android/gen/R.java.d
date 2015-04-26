e3ba48a75713d88bc4d60e83a87c80a4714c81cb
ColinGilbert <colingilbert86@gmail.com>
2015-04-26 06:22:11 -0600

    Modify .gitignores

--- engine/android/gen/R.java.d
+++
@@ -1,6 +0,0 @@
-/home/noob/noobgame/engine/android/gen/net/noobwerkz/sampleapp/R.java \
- : /home/noob/noobgame/engine/android/res/drawable-xhdpi/ic_launcher.png \
-/home/noob/noobgame/engine/android/res/values/strings.xml \
-/home/noob/noobgame/engine/android/res/drawable-hdpi/ic_launcher.png \
-/home/noob/noobgame/engine/android/res/drawable-mdpi/ic_launcher.png \
-/home/noob/noobgame/engine/android/bin/AndroidManifest.xml \
--- engine/android/gen/net/noobwerkz/sampleapp/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package net.noobwerkz.sampleapp;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
