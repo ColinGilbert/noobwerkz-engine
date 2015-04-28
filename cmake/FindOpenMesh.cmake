83486e0b5fc22dd902ea949b9012f406cc7cccfc
Jan Möbius <moebius@cs.rwth-aachen.de>
2015-04-28 11:33:32 +0000

    Relicense to BSD
    git-svn-id: http://www.openmesh.org/svnrepo/OpenMesh/trunk@1252 fdac6126-5c0c-442c-9429-916003d36597

--- cmake/FindOpenMesh.cmake
+++ cmake/FindOpenMesh.cmake
@@ -7,43 +7,50 @@
 # OPENMESH_LIBRARIES       - Link these to use OPENMESH
 # OPENMESH_LIBRARY_DIR     - directory where the libraries are included
 #
-# Copyright 2014 Computer Graphics Group, RWTH Aachen University
+# Copyright 2015 Computer Graphics Group, RWTH Aachen University
 # Authors: Jan Möbius <moebius@cs.rwth-aachen.de>
 #          Hans-Christian Ebke <ebke@cs.rwth-aachen.de>
 #
-# This file is part of OpenMesh.
+#
 #===========================================================================
 #                                                                           
 #                               OpenMesh                                    
-#      Copyright (C) 2001-2014 by Computer Graphics Group, RWTH Aachen      
-#                           www.openmesh.org                                
+#           Copyright (c) 2001-2015, RWTH-Aachen University                 
+#           Department for Computer Graphics and Multimedia                 
+#                          All rights reserved.                             
+#                            www.openmesh.org                               
 #                                                                           
 #---------------------------------------------------------------------------
-#  This file is part of OpenMesh.                                           
+# This file is part of OpenMesh.                                            
+#---------------------------------------------------------------------------
 #                                                                           
-#  OpenMesh is free software: you can redistribute it and/or modify         
-#  it under the terms of the GNU Lesser General Public License as           
-#  published by the Free Software Foundation, either version 3 of           
-#  the License, or (at your option) any later version with the              
-#  following exceptions:                                                    
+# Redistribution and use in source and binary forms, with or without        
+# modification, are permitted provided that the following conditions        
+# are met:                                                                  
 #                                                                           
-#  If other files instantiate templates or use macros                       
-#  or inline functions from this file, or you compile this file and         
-#  link it with other files to produce an executable, this file does        
-#  not by itself cause the resulting executable to be covered by the        
-#  GNU Lesser General Public License. This exception does not however       
-#  invalidate any other reasons why the executable file might be            
-#  covered by the GNU Lesser General Public License.                        
+# 1. Redistributions of source code must retain the above copyright notice, 
+#    this list of conditions and the following disclaimer.                  
 #                                                                           
-#  OpenMesh is distributed in the hope that it will be useful,              
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of           
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            
-#  GNU Lesser General Public License for more details.                      
+# 2. Redistributions in binary form must reproduce the above copyright      
+#    notice, this list of conditions and the following disclaimer in the    
+#    documentation and/or other materials provided with the distribution.   
 #                                                                           
-#  You should have received a copy of the GNU LesserGeneral Public          
-#  License along with OpenMesh.  If not,                                    
-#  see <http://www.gnu.org/licenses/>.                                      
+# 3. Neither the name of the copyright holder nor the names of its          
+#    contributors may be used to endorse or promote products derived from   
+#    this software without specific prior written permission.               
 #                                                                           
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS       
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED 
+# TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A           
+# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER 
+# OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,  
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,       
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR        
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF    
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING      
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS        
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.              
+#                                                                            
 #===========================================================================
 
 cmake_minimum_required(VERSION 2.8.9)
