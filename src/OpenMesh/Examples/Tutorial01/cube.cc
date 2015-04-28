77f93601977d70850a2b4e7d90b5dab5cfadda5b
Jan Möbius <moebius@cs.rwth-aachen.de>
2015-04-28 13:12:40 +0000

    Fixed bad search and replace results
    git-svn-id: http://www.openmesh.org/svnrepo/OpenMesh/trunk@1259 fdac6126-5c0c-442c-9429-916003d36597

--- src/OpenMesh/Examples/Tutorial01/cube.cc
+++ src/OpenMesh/Examples/Tutorial01/cube.cc
@@ -1,12 +1,4 @@
-//=============================================================================
-//                                                                            
-//                               OpenMesh                                     
-//      Copyright (C) 2001-2005 by Computer Graphics Group, RWTH Aachen       
-//                           www.openmesh.org                                 
-//                                                                            
-//-----------------------------------------------------------------------------
-//                                                                            
-//                                /* ========================================================================= *
+/* ========================================================================= *
  *                                                                           *
  *                               OpenMesh                                    *
  *           Copyright (c) 2001-2015, RWTH-Aachen University                 *
@@ -45,144 +37,14 @@
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS        *
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.              *
  *                                                                           *
- * ========================================================================= */                                     
-//                                                                            
-//   This library is free software; you can redistribute it and/or modify it 
-//   under the terms of the GNU Library General Public /* ========================================================================= *
+ * ========================================================================= */
+
+/*===========================================================================*\
  *                                                                           *
- *                               OpenMesh                                    *
- *           Copyright (c) 2001-2015, RWTH-Aachen University                 *
- *           Department of Computer Graphics and Multimedia                  *
- *                          All rights reserved.                             *
- *                            www.openmesh.org                               *
+ *   $Revision: 1258 $                                                       *
+ *   $Date: 2015-04-28 15:07:46 +0200 (Di, 28 Apr 2015) $                    *
  *                                                                           *
- *---------------------------------------------------------------------------*
- * This file is part of OpenMesh.                                            *
- *---------------------------------------------------------------------------*
- *                                                                           *
- * Redistribution and use in source and binary forms, with or without        *
- * modification, are permitted provided that the following conditions        *
- * are met:                                                                  *
- *                                                                           *
- * 1. Redistributions of source code must retain the above copyright notice, *
- *    this list of conditions and the following disclaimer.                  *
- *                                                                           *
- * 2. Redistributions in binary form must reproduce the above copyright      *
- *    notice, this list of conditions and the following disclaimer in the    *
- *    documentation and/or other materials provided with the distribution.   *
- *                                                                           *
- * 3. Neither the name of the copyright holder nor the names of its          *
- *    contributors may be used to endorse or promote products derived from   *
- *    this software without specific prior written permission.               *
- *                                                                           *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS       *
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED *
- * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A           *
- * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER *
- * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,  *
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,       *
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR        *
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF    *
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING      *
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS        *
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.              *
- *                                                                           *
- * ========================================================================= */ as published  
-//   by the Free Software Foundation, version 2.                             
-//                                                                             
-//   This library is distributed in the hope that it will be useful, but       
-//   WITHOUT ANY WARRANTY; without even the implied warranty of                
-//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU         
-//   Library General Public /* ========================================================================= *
- *                                                                           *
- *                               OpenMesh                                    *
- *           Copyright (c) 2001-2015, RWTH-Aachen University                 *
- *           Department of Computer Graphics and Multimedia                  *
- *                          All rights reserved.                             *
- *                            www.openmesh.org                               *
- *                                                                           *
- *---------------------------------------------------------------------------*
- * This file is part of OpenMesh.                                            *
- *---------------------------------------------------------------------------*
- *                                                                           *
- * Redistribution and use in source and binary forms, with or without        *
- * modification, are permitted provided that the following conditions        *
- * are met:                                                                  *
- *                                                                           *
- * 1. Redistributions of source code must retain the above copyright notice, *
- *    this list of conditions and the following disclaimer.                  *
- *                                                                           *
- * 2. Redistributions in binary form must reproduce the above copyright      *
- *    notice, this list of conditions and the following disclaimer in the    *
- *    documentation and/or other materials provided with the distribution.   *
- *                                                                           *
- * 3. Neither the name of the copyright holder nor the names of its          *
- *    contributors may be used to endorse or promote products derived from   *
- *    this software without specific prior written permission.               *
- *                                                                           *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS       *
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED *
- * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A           *
- * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER *
- * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,  *
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,       *
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR        *
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF    *
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING      *
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS        *
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.              *
- *                                                                           *
- * ========================================================================= */ for more details.                          
-//                                                                            
-//   You should have received a copy of the GNU Library General Public         
-//   /* ========================================================================= *
- *                                                                           *
- *                               OpenMesh                                    *
- *           Copyright (c) 2001-2015, RWTH-Aachen University                 *
- *           Department of Computer Graphics and Multimedia                  *
- *                          All rights reserved.                             *
- *                            www.openmesh.org                               *
- *                                                                           *
- *---------------------------------------------------------------------------*
- * This file is part of OpenMesh.                                            *
- *---------------------------------------------------------------------------*
- *                                                                           *
- * Redistribution and use in source and binary forms, with or without        *
- * modification, are permitted provided that the following conditions        *
- * are met:                                                                  *
- *                                                                           *
- * 1. Redistributions of source code must retain the above copyright notice, *
- *    this list of conditions and the following disclaimer.                  *
- *                                                                           *
- * 2. Redistributions in binary form must reproduce the above copyright      *
- *    notice, this list of conditions and the following disclaimer in the    *
- *    documentation and/or other materials provided with the distribution.   *
- *                                                                           *
- * 3. Neither the name of the copyright holder nor the names of its          *
- *    contributors may be used to endorse or promote products derived from   *
- *    this software without specific prior written permission.               *
- *                                                                           *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS       *
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED *
- * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A           *
- * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER *
- * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,  *
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,       *
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR        *
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF    *
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING      *
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS        *
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.              *
- *                                                                           *
- * ========================================================================= */ along with this library; if not, write to the Free Software       
-//   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.                 
-//                                                                            
-//-----------------------------------------------------------------------------
-//                                                                            
-//   $Revision: 83 $
-//   $Date: 2009-02-27 17:31:45 +0100 (Fri, 27 Feb 2009) $
-//                                                                            
-//=============================================================================
+ \*===========================================================================*/
 
 
 #include <iostream>
