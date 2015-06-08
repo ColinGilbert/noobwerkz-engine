23fb3781d424eb917688750df83d1784c6532a8a
Jan Möbius <moebius@cs.rwth-aachen.de>
2015-06-08 13:43:05 +0000

    Extended vector test for operator ==
    git-svn-id: http://www.openmesh.org/svnrepo/OpenMesh/trunk@1277 fdac6126-5c0c-442c-9429-916003d36597

--- src/Python/Unittests/test_vector_type.py
+++ src/Python/Unittests/test_vector_type.py
@@ -5,7 +5,7 @@ class VectorTest(unittest.TestCase):
 
     def test_compute_triangle_surface_with_cross_product(self):
         # vec1
-        #  x
+        #  y
         #  |
         #  |
         #  |
@@ -19,6 +19,22 @@ class VectorTest(unittest.TestCase):
 
         area = 0.5 * (vec1 % vec2).norm()
         self.assertEqual(area, 0.5)
+
+    def test_equality_operator_vec3d(self):
+        vec1 = openmesh.Vec3d(0.0, 1.0, 0.0)
+        vec2 = openmesh.Vec3d(1.0, 0.0, 0.0)
+        vec3 = openmesh.Vec3d(1.0, 0.0, 0.0)
+        
+        self.assertFalse(vec1==vec2)
+        self.assertTrue(vec3==vec2)
+        
+    def test_equality_operator_vec3f(self):
+        vec1 = openmesh.Vec3f(0.0, 1.0, 0.0)
+        vec2 = openmesh.Vec3f(1.0, 0.0, 0.0)
+        vec3 = openmesh.Vec3f(1.0, 0.0, 0.0)
+        
+        self.assertFalse(vec1==vec2)
+        self.assertTrue(vec3==vec2)     
 
     def test_abs_test(self):
         vec1 = openmesh.Vec3d(0.5, 0.5, -0.5)
