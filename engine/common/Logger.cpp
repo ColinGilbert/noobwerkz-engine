fedec14909cdbf5441dea4d07b894303cb7c925e
Colin <colingilbert86@gmail.com>
2015-07-17 19:13:35 -0600

    Switch to debug build

--- engine/common/Logger.cpp
+++ engine/common/Logger.cpp
@@ -1,11 +1,12 @@
 #include "Logger.hpp"
-
+#include <iostream>
 void logger::log(const std::string& message)
-		{
-			asio::ip::tcp::iostream s;
-			s.connect(SERVER_ADDRESS, SERVER_PORT);
-			if (s)
-			{
-				s << message << std::flush;
-			}
+{
+//			asio::ip::tcp::iostream s;
+//			s.connect(SERVER_ADDRESS, SERVER_PORT);
+//			if (s)
+//			{
+//				s << message << std::flush;
+//			}
+std::cout << message << std::endl;
 }
