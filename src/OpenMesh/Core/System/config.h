68fe500f18279d4d5d4c4152f4744201bd6afefc
Jan Möbius <moebius@cs.rwth-aachen.de>
2015-07-27 07:05:50 +0000

    Moved main version to 4.2
    git-svn-id: http://www.openmesh.org/svnrepo/OpenMesh/trunk@1322 fdac6126-5c0c-442c-9429-916003d36597

--- src/OpenMesh/Core/System/config.h
+++ src/OpenMesh/Core/System/config.h
@@ -64,7 +64,7 @@
 
 // ----------------------------------------------------------------------------
 
-#define OM_VERSION 0x40100
+#define OM_VERSION 0x40200
 
 // only defined, if it is a beta version
 //#define OM_VERSION_BETA 4
