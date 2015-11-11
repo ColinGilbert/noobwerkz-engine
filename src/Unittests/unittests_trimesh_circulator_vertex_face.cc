bbf51e4cc2236d8ced7d5d5d14a267ecf9541897
Jan Möbius <moebius@cs.rwth-aachen.de>
2015-11-11 14:43:32 +0100

    Fixed warnings in unit tests due to unused variables

--- src/Unittests/unittests_trimesh_circulator_vertex_face.cc
+++ src/Unittests/unittests_trimesh_circulator_vertex_face.cc
@@ -475,7 +475,8 @@ TEST_F(OpenMeshTrimeshCirculatorVertexFace, CWAndCCWCheck) {
   face_vhandles.push_back(vhandle[0]);
   face_vhandles.push_back(vhandle[1]);
   face_vhandles.push_back(vhandle[2]);
-  Mesh::FaceHandle fh0 = mesh_.add_face(face_vhandles);
+  
+  mesh_.add_face(face_vhandles);
 
   face_vhandles.clear();
 
