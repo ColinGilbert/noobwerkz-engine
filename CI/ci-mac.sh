6d6b02680cd8a9ce76f29914963098e417ccb50c
Jan Möbius <moebius@cs.rwth-aachen.de>
2015-11-11 14:30:00 +0100

    First version for mac gitlab-ci integration

---
+++ CI/ci-mac.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+if [ $(uname) == Darwin ]; then
+        PATH=$PATH:/opt/local/bin
+        export PATH
+fi
+
+#########################################
+# Build release version
+#########################################
+
+if [ ! -d build-release ]; then
+  mkdir build-release
+fi
+
+cd build-release
+
+cmake -DCMAKE_BUILD_TYPE=Release -DOPENMESH_BUILD_UNIT_TESTS=TRUE ../
+
+#build it
+make
+
+#build the unit tests
+make unittests
+
+
+#########################################
+# Run Release Unittests
+#########################################
+cd Unittests
+
+#execute tests
+./unittests --gtest_color=yes --gtest_output=xml
+
+cd ..
+cd ..
+
+
+#########################################
+# Build Debug version and Unittests
+#########################################
+
+if [ ! -d build-debug ]; then
+  mkdir build-debug
+fi
+
+cd build-debug
+
+cmake -DCMAKE_BUILD_TYPE=Debug -DOPENMESH_BUILD_UNIT_TESTS=TRUE ../
+
+#build the unit tests
+make unittests
+
+
+#########################################
+# Run Debug Unittests
+#########################################
+
+cd Unittests
+
+# Run the unittests
+./unittests --gtest_color=yes --gtest_output=xml
+
+
