a04d46d6126d6b1c0aea4ee8fcb09ef54e6aa4e0
Jan Möbius <moebius@cs.rwth-aachen.de>
2015-11-25 16:09:22 +0100

    Enabled python tests on mac

--- CI/ci-mac.sh
+++ CI/ci-mac.sh
@@ -28,7 +28,7 @@ fi
 
 cd build-release
 
-cmake -DCMAKE_BUILD_TYPE=Release -DOPENMESH_BUILD_UNIT_TESTS=TRUE $OPTIONS ../
+cmake -DCMAKE_BUILD_TYPE=Release -DOPENMESH_BUILD_UNIT_TESTS=TRUE -DSTL_VECTOR_CHECKS=ON -DOPENMESH_BUILD_PYTHON_UNIT_TESTS=ON $OPTIONS ../
 
 #build it
 make
@@ -45,6 +45,14 @@ cd Unittests
 #execute tests
 ./unittests --gtest_color=yes --gtest_output=xml
 
+# Execute Python unittests
+cd Python-Unittests
+
+rm -f openmesh.so
+cp ../Build/python/openmesh.so .
+python -m unittest discover -v
+
+cd ..
 cd ..
 cd ..
 
@@ -59,7 +67,7 @@ fi
 
 cd build-debug
 
-cmake -DCMAKE_BUILD_TYPE=Debug -DOPENMESH_BUILD_UNIT_TESTS=TRUE $OPTIONS ../
+cmake -DCMAKE_BUILD_TYPE=Debug -DOPENMESH_BUILD_UNIT_TESTS=TRUE -DSTL_VECTOR_CHECKS=ON -DOPENMESH_BUILD_PYTHON_UNIT_TESTS=ON $OPTIONS ../
 
 #build the unit tests
 make unittests
@@ -74,4 +82,9 @@ cd Unittests
 # Run the unittests
 ./unittests --gtest_color=yes --gtest_output=xml
 
+# Execute Python unittests
+cd Python-Unittests
 
+rm -f openmesh.so
+cp ../Build/python/openmesh.so .
+python -m unittest discover -v
