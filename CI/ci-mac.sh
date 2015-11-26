f8d3b8fe55f8f0530958bb2d311db9a91b30998a
Jan Möbius <moebius@cs.rwth-aachen.de>
2015-11-26 12:20:12 +0100

    Fixed mac ci script

--- CI/ci-mac.sh
+++ CI/ci-mac.sh
@@ -45,6 +45,7 @@ cd Unittests
 #execute tests
 ./unittests --gtest_color=yes --gtest_output=xml
 
+cd ..
 # Execute Python unittests
 cd Python-Unittests
 
@@ -52,7 +53,6 @@ rm -f openmesh.so
 cp ../Build/python/openmesh.so .
 python -m unittest discover -v
 
-cd ..
 cd ..
 cd ..
 
@@ -69,6 +69,9 @@ cd build-debug
 
 cmake -DCMAKE_BUILD_TYPE=Debug -DOPENMESH_BUILD_UNIT_TESTS=TRUE -DSTL_VECTOR_CHECKS=ON -DOPENMESH_BUILD_PYTHON_UNIT_TESTS=ON $OPTIONS ../
 
+#build it
+make
+
 #build the unit tests
 make unittests
 
@@ -81,6 +84,8 @@ cd Unittests
 
 # Run the unittests
 ./unittests --gtest_color=yes --gtest_output=xml
+
+cd ..
 
 # Execute Python unittests
 cd Python-Unittests
