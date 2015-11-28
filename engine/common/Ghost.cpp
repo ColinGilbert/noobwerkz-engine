460db9bb7fd2d0c95606a4798c7d4129efda340b
Colin <colingilbert86@gmail.com>
2015-11-28 17:23:42 -0600

    Even more cleanups

--- engine/common/Ghost.cpp
+++
@@ -1,8 +0,0 @@
-/*
-std::vector<rp3d::ContactManifold*> noob::ghost:get_contact_manifolds() const
-{
-	const ContactManifoldListElement* manifold_list = getContactManifoldsList() const;
-	std::vector<ContactManifold*> results;
-	results.push_back(manifold_list.contactManifold);
-}
-*/
