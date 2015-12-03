3ac9557dd404054d955fd6c8b9fec1a9af972142
Jan Möbius <moebius@cs.rwth-aachen.de>
2015-12-03 10:40:32 +0100

    Removed stl vector checks from python unittest build on linux

--- CI/ci-linux.sh
+++ CI/ci-linux.sh
@@ -28,16 +28,21 @@ elif [ "$LANGUAGE" == "C++11" ]; then
   BUILDPATH="$BUILDPATH-cpp11"  
 fi  
 
-#########################################
 
-# Make release build folder
-if [ ! -d build-release-$BUILDPATH ]; then
-  mkdir build-release-$BUILDPATH
+echo "\n"
+echo "\n"
+echo "======================================================================"
+echo "Building Release version with vectorchecks enabled"
+echo "====================================================================== \n"
+
+
+if [ ! -d build-release-$BUILDPATH-Vector-Checks ]; then
+  mkdir build-release-$BUILDPATH-Vector-Checks
 fi
 
-cd build-release-$BUILDPATH
+cd build-release-$BUILDPATH-Vector-Checks
 
-cmake -DCMAKE_BUILD_TYPE=Release -DOPENMESH_BUILD_UNIT_TESTS=TRUE -DSTL_VECTOR_CHECKS=ON -DOPENMESH_BUILD_PYTHON_UNIT_TESTS=ON $OPTIONS ../
+cmake -DCMAKE_BUILD_TYPE=Release -DOPENMESH_BUILD_UNIT_TESTS=TRUE -DSTL_VECTOR_CHECKS=ON -DOPENMESH_BUILD_PYTHON_UNIT_TESTS=OFF $OPTIONS ../
 
 #build it
 make
@@ -45,28 +50,96 @@ make
 #build the unit tests
 make unittests
 
+echo "\n"
+echo "\n"
+echo "======================================================================"
+echo "Running unittests Release version with vectorchecks enabled"
+echo "====================================================================== \n"
 
-#########################################
-# Run Release Unittests
-#########################################
+
 cd Unittests
 
 #execute tests
 ./unittests --gtest_color=yes --gtest_output=xml
 
 cd ..
+cd ..
+
+echo "\n"
+echo "\n"
+echo "======================================================================"
+echo "Building Release version with vectorchecks disabled for python tests"
+echo "====================================================================== \n"
+
+if [ ! -d build-release-$BUILDPATH ]; then
+  mkdir build-release-$BUILDPATH
+fi
+
+cd build-release-$BUILDPATH
+
+cmake -DCMAKE_BUILD_TYPE=Release -DOPENMESH_BUILD_PYTHON_UNIT_TESTS=ON -DBUILD_APPS=OFF $OPTIONS ../
+
+#build it
+make
+
+echo "\n"
+echo "\n"
+echo "======================================================================"
+echo "Running Python unittests Release version "
+echo "====================================================================== \n"
+
 
 # Execute Python unittests
 cd Python-Unittests
+
 python -m unittest discover -v
 
 cd ..
 cd ..
 
 
-#########################################
-# Build Debug version and Unittests
-#########################################
+
+echo "\n"
+echo "\n"
+echo "======================================================================"
+echo "Building Debug version with vectorchecks enabled"
+echo "====================================================================== \n"
+
+
+if [ ! -d build-debug-$BUILDPATH-Vector-Checks ]; then
+  mkdir build-debug-$BUILDPATH-Vector-Checks
+fi
+
+cd build-debug-$BUILDPATH-Vector-Checks
+
+cmake -DCMAKE_BUILD_TYPE=Debug -DOPENMESH_BUILD_UNIT_TESTS=TRUE -DSTL_VECTOR_CHECKS=ON -DOPENMESH_BUILD_PYTHON_UNIT_TESTS=OFF $OPTIONS ../
+
+#build it
+make
+
+#build the unit tests
+make unittests
+
+echo "\n"
+echo "\n"
+echo "======================================================================"
+echo "Running unittests Debug version with vectorchecks enabled"
+echo "====================================================================== \n"
+
+
+cd Unittests
+
+#execute tests
+./unittests --gtest_color=yes --gtest_output=xml
+
+cd ..
+cd ..
+
+echo "\n"
+echo "\n"
+echo "======================================================================"
+echo "Building Debug version with vectorchecks disabled for python tests"
+echo "====================================================================== \n"
 
 if [ ! -d build-debug-$BUILDPATH ]; then
   mkdir build-debug-$BUILDPATH
@@ -74,30 +147,19 @@ fi
 
 cd build-debug-$BUILDPATH
 
-cmake -DCMAKE_BUILD_TYPE=Debug -DOPENMESH_BUILD_UNIT_TESTS=TRUE -DSTL_VECTOR_CHECKS=ON -DOPENMESH_BUILD_PYTHON_UNIT_TESTS=ON $OPTIONS ../
+cmake -DCMAKE_BUILD_TYPE=DEBUG -DOPENMESH_BUILD_PYTHON_UNIT_TESTS=ON -DBUILD_APPS=OFF $OPTIONS ../
 
 #build it
 make
 
-#build the unit tests
-make unittests
+echo "\n"
+echo "\n"
+echo "======================================================================"
+echo "Running Python unittests Debug version "
+echo "====================================================================== \n"
 
-
-#########################################
-# Run Debug Unittests
-#########################################
-
-cd Unittests
-
-# Run the unittests
-./unittests --gtest_color=yes --gtest_output=xml
-
-cd ..
 
 # Execute Python unittests
 cd Python-Unittests
+
 python -m unittest discover -v
-
-cd ..
-cd ..
-
