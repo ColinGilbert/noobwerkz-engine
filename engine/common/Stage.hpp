2c5f816ebde312f388fd768242b6b9481b4cb3a1
Colin <colingilbert86@gmail.com>
2015-12-13 00:41:11 -0600

    More cleanups

--- engine/common/Stage.hpp
+++ engine/common/Stage.hpp
@@ -7,10 +7,12 @@
 #include <tuple>
 #include <list>
 #include <forward_list>
+
 #include <boost/variant.hpp>
+//#include <boost/intrusive_ptr.hpp>
+
 #include <btBulletDynamicsCommon.h>
 #include "es/storage.hpp"
-
 #include "Graphics.hpp"
 #include "MathFuncs.hpp"
 #include "VoxelWorld.hpp"
@@ -28,23 +30,9 @@
 #include "Scenery.hpp"
 #include "Body.hpp"
 #include "Shape.hpp"
-// #include "Component.hpp"
 
 namespace noob
 {
-	// typedef noob::component<std::unique_ptr<noob::basic_model>> basic_model_component;	
-	// typedef noob::component<std::unique_ptr<noob::animated_model>> animated_model_component;
-	// typedef noob::component<std::unique_ptr<noob::basic_mesh>> mesh_component;
-	// typedef noob::component<std::unique_ptr<noob::skeletal_anim>> skeleton_component;
-	// typedef noob::component<noob::light> light_component;
-	// typedef noob::component<noob::reflection> reflection_component;
-	// typedef noob::component<noob::prepared_shaders::info> shader_component;
-	// typedef noob::component<std::unique_ptr<noob::shape>> shape_component;
-	// typedef noob::component<noob::body> body_component;
-	// typedef noob::component<noob::prop> prop_component;	
-	// typedef noob::component<noob::actor> actor_component;
-	// typedef noob::component<noob::scenery> scenery_component;
-
 	class stage
 	{
 		public:
@@ -70,28 +58,15 @@ namespace noob
 			// reflection_component::handle reflection(const noob::reflection&);
 			// shader_component::handle shader(const noob::prepared_shaders::info&, const std::string& name);
 			
-			// These cache the shape for reuse, as they are simple parametrics.
+			// These cache the shape for quick reuse.
 			// noob::shape sphere(float r);
 			// noob::shape box(float x, float y, float z);
 			// noob::shape cylinder(float r, float h);
 			// noob::shape cone(float r, float h);
 			// noob::shape capsule(float r, float h);
 			// noob::shape plane(const noob::vec3& normal, float offset);
-
 			// noob::shape hull(const std::vector<noob::vec3>& points, const std::string& name);
 			// noob::shape trimesh(const noob::basic_mesh& mesh, const std::string& name);
-
-			// scenery_component sceneries;
-			// prop_component props;
-			// actor_component actors;
-			// light_component lights;
-			// reflection_component reflections;
-			// shader_component shaders;
-			// shape_component shapes;
-			// body_component bodies;
-			// basic_model_component basic_models;
-			// animated_model_component animated_models;
-			// skeleton_component skeletons;
 
 		protected:
 
@@ -113,6 +88,5 @@ namespace noob
 
 			es::storage pool;
 
-			//auto s;
 	};
 }
