87ad4214cbc98278512ddf1a41931fd21134fc16
Colin <colingilbert86@gmail.com>
2016-01-07 22:12:51 -0600

    Add smartass comments

--- engine/common/ShaderUniforms.hpp
+++ engine/common/ShaderUniforms.hpp
@@ -1,4 +1,6 @@
-// A full shader uniform block. This is to get away from the machinery that uses boost::variant and some of the crazy namespacing that comes up far too often when writing client-side code.
+// A shader uniform block. This is to get away from the machinery that uses boost::variant and some of the crazy namespacing and contortionism that comes up far too often when writing client-side code.
+// Still WIP after a brave slog and a clean retreat... :P
+// TODO: Implement.
 
 #pragma once
 
@@ -8,6 +10,4 @@ namespace noob
 	{
 
 	};
-
-
 }
