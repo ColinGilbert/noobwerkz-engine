f80d2c6db3390e2b90a44c8f0e38427379915740
Colin <colingilbert86@gmail.com>
2016-02-07 22:11:49 -0600

    Remove all brl-cad due to licensing :(

--- engine/common/CSG.cpp
+++ engine/common/CSG.cpp
@@ -1,40 +1,18 @@
 #include "CSG.hpp"
 
-#define V3ARGSIN(a)       (a)[X]/25.4, (a)[Y]/25.4, (a)[Z]/25.4
-#define VSETIN(a, b)	{\
-	(a)[X] = (b)[X]/25.4; \
-	(a)[Y] = (b)[Y]/25.4; \
-	(a)[Z] = (b)[Z]/25.4; \
-    }
-
 void noob::csg::init()
 {
-    tree_state = rt_initial_tree_state;
-    tree_state.ts_tol = &calculation_tolerance;
-    tree_state.ts_ttol = &tesselation_tolerance;
-    tree_state.ts_m = &csg_model;
 
-    // Set up tessellation tolerance defaults
-    tesselation_tolerance.magic = RT_TESS_TOL_MAGIC;
-    // Defaults, updated by command line options.
-    tesselation_tolerance.abs = 0.0;
-    tesselation_tolerance.rel = 0.01;
-    tesselation_tolerance.norm = 0.0;
-
-    // Set up calculation tolerance defaults
-    // FIXME: These need to be improved
-    calculation_tolerance.magic = BN_TOL_MAGIC;
-    calculation_tolerance.dist = 0.0005;
-    calculation_tolerance.dist_sq = calculation_tolerance.dist * calculation_tolerance.dist;
-    calculation_tolerance.perp = 1e-6;
-    calculation_tolerance.para = 1 - calculation_tolerance.perp;
-
-    // init resources we might need
-    rt_init_resource(&rt_uniresource, 0, NULL);
 }
 
 
 noob::basic_mesh noob::csg::to_basic_mesh() const
+{
+
+}
+
+
+void noob::csg::shutdown()
 {
 
 }
--- engine/common/CSG.hpp
+++ engine/common/CSG.hpp
@@ -1,37 +1,18 @@
 #pragma once
 
 #include "BasicMesh.hpp"
-
-#include <brlcad/vmath.h>
-#include <brlcad/nmg.h>
-#include <brlcad/rtgeom.h>
-#include <brlcad/raytrace.h>
-
+#include "MathFuncs.hpp"
 
 namespace noob
 {
 	class csg
 	{
 		public:
-			csg() : verbose(false), NMG_debug(0), regions_tried(0), regions_converted(0), regions_written(0), total_polygons(0) {}
-			
 			void init();
+			void shutdown();
+
 			noob::basic_mesh to_basic_mesh() const;
 
 		protected:
-			// Members
-			bool verbose;
-			int NMG_debug, regions_tried, regions_converted, regions_written;
-			size_t total_polygons;
-
-			db_i *dbip;
-			rt_tess_tol tesselation_tolerance;
-			bn_tol calculation_tolerance;
-			model *csg_model;
-
-			db_tree_state tree_state;
-
-			// Functions
-			// 
 	};
 }
