21f796d1b7f1e00b57e0fe14b421d4f9252f2c6c
Colin <colingilbert86@gmail.com>
2016-09-23 15:50:41 -0600

    Begin refactoring Android into NativeActivity

--- engine/platform/droid/EngineDroid.cpp
+++ engine/platform/droid/EngineDroid.cpp
@@ -1,186 +1,423 @@
-#include "EngineDroid.hpp"
+#include <string>
 
-extern "C"
+#include <android/input.h>
+#include <android/log.h>
+#include <android/looper.h>
+#include <android/window.h>
+#include <android_native_app_glue.h>
+#include <android/native_window.h>
+
+#include <bgfx/bgfx.h>
+#include <bgfx/bgfxplatform.h>
+
+#include "Application.hpp"
+#include "Graphics.hpp"
+#include "NoobUtils.hpp"
+
+static ANativeWindow* droid_window;
+static android_app* droid_app;
+
+
+inline void android_set_window(ANativeWindow* _window)
 {
-	JNIEXPORT void JNICALL Java_net_noobwerkz_sampleapp_JNILib_OnInit(JNIEnv* env, jobject obj);
-	JNIEXPORT void JNICALL Java_net_noobwerkz_sampleapp_JNILib_OnShutdown(JNIEnv* env, jobject obj);
-	JNIEXPORT void JNICALL Java_net_noobwerkz_sampleapp_JNILib_OnResize(JNIEnv* env, jobject obj, jint iWidth, jint iHeight);
-	JNIEXPORT void JNICALL Java_net_noobwerkz_sampleapp_JNILib_OnFrame(JNIEnv* env, jobject obj);
-	JNIEXPORT void JNICALL Java_net_noobwerkz_sampleapp_JNILib_OnTouch(JNIEnv* env, jobject obj, int pointerID, float x, float y, int action);
-	JNIEXPORT void JNICALL Java_net_noobwerkz_sampleapp_JNILib_OnPause(JNIEnv* env, jobject obj);
-	JNIEXPORT void JNICALL Java_net_noobwerkz_sampleapp_JNILib_OnResume(JNIEnv* env, jobject obj);
-	JNIEXPORT void JNICALL Java_net_noobwerkz_sampleapp_JNILib_SetupArchiveDir(JNIEnv* env, jobject obj, jstring dir);
-	JNIEXPORT void JNICALL Java_net_noobwerkz_sampleapp_JNILib_Log(JNIEnv* env, jobject obj, jstring message);
-	JNIEXPORT void JNICALL Java_net_noobwerkz_sampleapp_JNILib_NativeSetSurface(JNIEnv* eng, jobject obj, jobject surface);
-};
-
-static std::unique_ptr<noob::application> app; // = nullptr;
-
-JNIEXPORT void JNICALL Java_net_noobwerkz_sampleapp_JNILib_OnInit(JNIEnv* env, jobject obj)
-{
-	noob::logger::log(noob::importance::INFO, "JNILib.OnInit()");
-
-	if (!app)
-	{
-		app = std::unique_ptr<noob::application>(new noob::application());
-	}
-
-	// http://stackoverflow.com/questions/32028810/android-how-to-get-the-preferred-native-audio-buffer-size-and-audio-sample-r/35507038#35507038
-	jclass audioSystem = env->FindClass("android/media/AudioSystem");
-	jmethodID method = env->GetStaticMethodID(audioSystem, "getPrimaryOutputSamplingRate", "()I");
-	jint nativeOutputSampleRate = env->CallStaticIntMethod(audioSystem, method);
-	method = env->GetStaticMethodID(audioSystem, "getPrimaryOutputFrameCount", "()I");
-	jint nativeBufferLength = env->CallStaticIntMethod(audioSystem, method);
-
-}
-
-JNIEXPORT void JNICALL Java_net_noobwerkz_sampleapp_JNILib_OnShutdown(JNIEnv* env, jobject obj)
-{
-	noob::logger::log(noob::importance::INFO, "JNILib.OnShutdown()");
-
-
-	if(app)
-	{
-		// delete app;
-		app = nullptr;
-	}
-}
-
-JNIEXPORT void JNICALL Java_net_noobwerkz_sampleapp_JNILib_OnResize(JNIEnv* env, jobject obj, jint width, jint height)
-{
-	noob::logger::log(noob::importance::INFO, "JNILib.OnResize()");
-
-	_height = height;
-	_width = width;
-	/*
-	   auto last_context = current_context;
-	   current_context = eglGetCurrentContext();
-	   {
-	   std::stringstream ss;
-	   ss << "Last context: " << last_context;
-	   noob::logger::log(noob::importance::INFO, ss.str());
-	   }
-
-	   {
-	   std::stringstream ss;
-	   ss << "Updated context: " << current_context;
-	   noob::logger::log(noob::importance::INFO, ss.str());
-	   }
-
-	   if (current_context != last_context)
-	   {
-	   if (last_context != 0)
-	   {
-	   noob::logger::log(noob::importance::INFO, "bgfx::shutdown()");
-	   bgfx::shutdown();
-	   }
-
-	   noob::logger::log(noob::importance::INFO, "About to set BGFX platform data");
-
-	   bgfx::PlatformData pd;
-	   pd.ndt = NULL; // Native display type
-	   pd.nwh = NULL; // Native window handle
-	   pd.context = (void*)(uintptr_t)current_context; // eglGetCurrentContext(); // Pass the EGLContext created by GLSurfaceView.
-	   pd.backbuffer = NULL; // GL backbuffer, or D3D render target view
-
-	   bgfx::setPlatformData(pd);
-
-	   noob::logger::log(noob::importance::INFO, "BGFX platform data reset!");
-	   */
-	ANativeWindow* window = ANativeWindow_fromSurface(env, obj);
-
 	bgfx::PlatformData pd;
 	pd.ndt          = NULL;
-	pd.nwh          = window;
+	pd.nwh          = _window;
 	pd.context      = NULL;
 	pd.backBuffer   = NULL;
 	pd.backBufferDS = NULL;
-
 	bgfx::setPlatformData(pd);
+}
+#include <jni.h>
+#include <errno.h>
 
-	bgfx::init();
-	noob::graphics& gfx = noob::graphics::get_instance();
-	gfx.init(width, height);
+#include <EGL/egl.h>
+#include <GLES/gl.h>
 
-	if (app)
-	{
-		app->init();
-	}
+#include <android/sensor.h>
+#include <android/log.h>
+#include <android_native_app_glue.h>
 
-	app->window_resize(width, height);
+#define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO, "native-activity", __VA_ARGS__))
+#define LOGW(...) ((void)__android_log_print(ANDROID_LOG_WARN, "native-activity", __VA_ARGS__))
+
+
+// Our saved state data.
+struct saved_state {
+	float angle;
+	int32_t x;
+	int32_t y;
+};
+
+
+// Shared state for our app.
+struct engine
+{
+	struct android_app* app;
+
+	ASensorManager* sensor_manager;
+	const ASensor* accelerometer_sensor;
+	ASensorEventQueue* sensor_event_queue;
+
+	bool animating;
+	// EGLDisplay display;
+	// EGLSurface surface;
+	// EGLContext context;
+	int32_t width;
+	int32_t height;
+	struct saved_state state;
+};
+
+// Initialize an EGL context for the current display.
+static void init_display(struct engine* engine) {
+	// initialize OpenGL ES and EGL
+
+
 }
 
-JNIEXPORT void JNICALL Java_net_noobwerkz_sampleapp_JNILib_OnFrame(JNIEnv* env, jobject obj)
+/**
+ * Just the current frame in the display.
+ */
+static void draw_frame(struct engine* engine)
 {
-	app->step();
-	noob::graphics& gfx = noob::graphics::get_instance();
-	gfx.frame(_width, _height);
+	// if (engine->display == NULL) {
+	// No display.
+	//	noob::logger::log(noob::importance::ERROR, "[NativeActivity] - Attempted to draw frame with inexistent display..."
+	//      return;
+	//}
+
+	// Just fill the screen with a color.
+	//glClearColor(((float)engine->state.x)/engine->width, engine->state.angle,
+	//      ((float)engine->state.y)/engine->height, 1);
+	//glClear(GL_COLOR_BUFFER_BIT);
+
+	//eglSwapBuffers(engine->display, engine->surface);
+
 }
 
-JNIEXPORT void JNICALL Java_net_noobwerkz_sampleapp_JNILib_OnTouch(JNIEnv* env, jobject obj, int pointerID, float x, float y, int action)
+/**
+ * Tear down the EGL context currently associated with the display.
+ */
+static void engine_term_display(struct engine* engine)
 {
-	if (app)
-	{
-		app->touch(pointerID, x, y, action);
-	}
-}
-
-JNIEXPORT void JNICALL Java_net_noobwerkz_sampleapp_JNILib_OnPause(JNIEnv* env, jobject obj)
-{
-	noob::logger::log(noob::importance::INFO, "JNILib.OnPause()");
-	// Pretty much a dead callback as interfering with the app pointer crashes
-}
-
-JNIEXPORT void JNICALL Java_net_noobwerkz_sampleapp_JNILib_OnResume(JNIEnv* env, jobject obj)
-{
-	noob::logger::log(noob::importance::INFO, "JNILib.OnResume()");
-
-	if(app)
-	{
-		app->resume();
-	}
-}
-
-std::string ConvertJString(JNIEnv* env, jstring str)
-{
-	if (!str) { return std::string(); }
-
-	const jsize len = env->GetStringUTFLength(str);
-	const char* strChars = env->GetStringUTFChars(str,(jboolean*)0);
-	std::string Result(strChars, len);
-
-	env->ReleaseStringUTFChars(str, strChars);
+	// if (engine->display != EGL_NO_DISPLAY)
 	// {
-	// std::stringstream ss;
-	// ss << "JNILib.ConvertJString(" << Result << ")";
-	// noob::logger::log(noob::importance::INFO, ss.str());
+	//	eglMakeCurrent(engine->display, EGL_NO_SURFACE, EGL_NO_SURFACE, EGL_NO_CONTEXT);
+	//	if (engine->context != EGL_NO_CONTEXT)
+	//	{
+	//		eglDestroyContext(engine->display, engine->context);
+	//	}
+	//	if (engine->surface != EGL_NO_SURFACE)
+	//	{
+	//		eglDestroySurface(engine->display, engine->surface);
+	//	}
+	//	eglTerminate(engine->display);
 	// }
-
-	return Result;
+	engine->animating = false;
+	// engine->display = EGL_NO_DISPLAY;
+	// engine->context = EGL_NO_CONTEXT;
+	// engine->surface = EGL_NO_SURFACE;
 }
 
-JNIEXPORT void JNICALL Java_net_noobwerkz_sampleapp_JNILib_SetupArchiveDir(JNIEnv * env, jobject obj, jstring dir)
+// Process the next input event.
+
+static int32_t handle_input(struct android_app* app, AInputEvent* event)
 {
-	const char* temp = env->GetStringUTFChars(dir, NULL);
-	archive_dir = std::string(temp);
-
-	noob::logger::log(noob::importance::INFO, noob::concat("JNILib.SetupArchiveDir(",  archive_dir, ")"));
-
-	if (app)
+	struct engine* engine = (struct engine*)app->userData;
+	if (AInputEvent_getType(event) == AINPUT_EVENT_TYPE_MOTION)
 	{
-		app->set_archive_dir(archive_dir);
+		// engine->animating = 1;
+		//engine->state.x = AMotionEvent_getX(event, 0);
+		//engine->state.y = AMotionEvent_getY(event, 0);
+		return 1;
+	}
+	return 0;
+}
+
+/**
+ * Process the next main command.
+ */
+static void handle_cmd(struct android_app* app, int32_t cmd)
+{
+	struct engine* engine = (struct engine*)app->userData;
+	switch (cmd)
+	{
+		case APP_CMD_SAVE_STATE:
+			// The system has asked us to save our current state.  Do so.
+			engine->app->savedState = malloc(sizeof(struct saved_state));
+			*((struct saved_state*)engine->app->savedState) = engine->state;
+			engine->app->savedStateSize = sizeof(struct saved_state);
+			break;
+		case APP_CMD_INIT_WINDOW:
+			// The window is being shown, get it ready.
+			if (engine->app->window != NULL)
+			{
+				init_display(engine);
+				draw_frame(engine);
+			}
+			break;
+		case APP_CMD_TERM_WINDOW:
+			// The window is being hidden or closed, clean it up.
+			engine_term_display(engine);
+			break;
+		case APP_CMD_GAINED_FOCUS:
+			// When our app gains focus, we start monitoring the accelerometer.
+			if (engine->accelerometer_sensor != NULL) {
+				ASensorEventQueue_enableSensor(engine->sensor_event_queue,
+						engine->accelerometer_sensor);
+				// We'd like to get 60 events per second (in us).
+				ASensorEventQueue_setEventRate(engine->sensor_event_queue,
+						engine->accelerometer_sensor, (1000L/60)*1000);
+			}
+			break;
+		case APP_CMD_LOST_FOCUS:
+			// When our app loses focus, we stop monitoring the accelerometer.
+			// This is to avoid consuming battery while not being used.
+			if (engine->accelerometer_sensor != NULL) {
+				ASensorEventQueue_disableSensor(engine->sensor_event_queue,
+						engine->accelerometer_sensor);
+			}
+			// Also stop animating.
+			engine->animating = 0;
+			draw_frame(engine);
+			break;
+	}
+}
+
+
+// This is the main entry point of a native application that is using android_native_app_glue.  It runs in its own thread with its own event loop for receiving input events and doing other things.
+
+void android_main(struct android_app* state)
+{
+	struct engine engine;
+
+	// Make sure glue isn't stripped.
+	app_dummy();
+
+	//    memset(&engine, 0, sizeof(engine));
+	state->userData = &engine;
+	state->onAppCmd = handle_cmd;
+	state->onInputEvent = handle_input;
+	engine.app = state;
+
+	// Prepare to monitor accelerometer
+	engine.sensor_manager = ASensorManager_getInstance();
+	engine.accelerometer_sensor = ASensorManager_getDefaultSensor(engine.sensor_manager, ASENSOR_TYPE_ACCELEROMETER);
+	engine.sensor_event_queue = ASensorManager_createEventQueue(engine.sensor_manager, state->looper, LOOPER_ID_USER, NULL, NULL);
+
+	if (state->savedState != NULL)
+	{
+		// We are starting with a previous saved state; restore from it.
+		engine.state = *(struct saved_state*)state->savedState;
 	}
 
+	// loop waiting for stuff to do.
+	while (true)
+	{
+		// Read all pending events.
+		int ident;
+		int events;
+		struct android_poll_source* source;
+
+		// If not animating, we will block forever waiting for events. If animating, we loop until all events are read, then continue to draw the next frame of animation.
+		while ((ident=ALooper_pollAll(engine.animating ? 0 : -1, NULL, &events, (void**)&source)) >= 0) // Colin's note: Ewww...
+		{
+
+			// Process this event.
+			if (source != NULL)
+			{
+				source->process(state, source);
+			}
+
+			// If a sensor has data, process it now.
+			if (ident == LOOPER_ID_USER)
+			{
+				if (engine.accelerometer_sensor != NULL)
+				{
+					ASensorEvent event;
+					while (ASensorEventQueue_getEvents(engine.sensor_event_queue, &event, 1) > 0)
+					{
+						noob::logger::log(noob::importance::INFO, noob::concat("[NativeActivity] Accelerometer: x=", noob::to_string(event.acceleration.x), " y=", noob::to_string(event.acceleration.y), " z=", noob::to_string(event.acceleration.z)));
+					}
+				}
+			}
+
+			// Check if we are exiting.
+			if (state->destroyRequested != 0)
+			{
+				engine_term_display(&engine);
+				return;
+			}
+		}
+
+		if (engine.animating)
+		{
+			// Drawing is throttled to the screen update rate, so there is no need to do timing here.
+			draw_frame(&engine);
+		}
+	}
 }
 
-JNIEXPORT void JNICALL Java_net_noobwerkz_sampleapp_JNILib_Log(JNIEnv* env, jobject obj, jstring message)
+/*
+   void handle_cmd(android_app* state, int32_t _cmd)
+   {
+   switch (_cmd)
+   {
+   case APP_CMD_INPUT_CHANGED:
+// Command from main thread: the AInputQueue has changed.  Upon processing
+// this command, android_app->inputQueue will be updated to the new queue
+// (or NULL).
+break;
+
+case APP_CMD_INIT_WINDOW:
+// Command from main thread: a new ANativeWindow is ready for use.  Upon
+// receiving this command, android_app->window will contain the new window
+// surface.
+if (droid_window != droid_app->window)
 {
-	const char* temp = env->GetStringUTFChars(message, NULL);
-	noob::logger::log(noob::importance::INFO, std::string(temp));
+droid_window = droid_app->window;
+android_set_window(droid_window);
+
+int32_t width  = ANativeWindow_getWidth(droid_window);
+int32_t height = ANativeWindow_getHeight(droid_window);
+
+noob::logger::log(noob::importance::INFO, noob::concat("ANativeWindow width = ", noob::to_string(width), ", height =", noob::to_string(height)));
+// WindowHandle defaultWindow = { 0 };
+//m_eventQueue.postSizeEvent(defaultWindow, width, height);
+
+//	if (!m_thread.isRunning() )
+//	{
+//		m_thread.init(MainThreadEntry::threadFunc, &m_mte);
+//	}
 }
+break;
 
+case APP_CMD_TERM_WINDOW:
+// Command from main thread: the existing ANativeWindow needs to be
+// terminated.  Upon receiving this command, android_app->window still
+// contains the existing window; after calling android_app_exec_cmd
+// it will be set to NULL.
+break;
 
-JNIEXPORT void JNICALL Java_net_noobwerkz_sampleapp_JNILib_NativeSetSurface(JNIEnv* env, jobject obj, jobject surface)
+case APP_CMD_WINDOW_RESIZED:
+// Command from main thread: the current ANativeWindow has been resized.
+// Please redraw with its new size.
+break;
+
+case APP_CMD_WINDOW_REDRAW_NEEDED:
+// Command from main thread: the system needs that the current ANativeWindow
+// be redrawn.  You should redraw the window before handing this to
+// android_app_exec_cmd() in order to avoid transient drawing glitches.
+break;
+
+case APP_CMD_CONTENT_RECT_CHANGED:
+// Command from main thread: the content area of the window has changed,
+// such as from the soft input window being shown or hidden.  You can
+// find the new content rect in android_app::contentRect.
+break;
+
+case APP_CMD_GAINED_FOCUS:
 {
-
+// Command from main thread: the app's activity window has gained
+// input focus.
+// WindowHandle defaultWindow = { 0 };
+// m_eventQueue.postSuspendEvent(defaultWindow, Suspend::WillResume);
+break;
 }
+
+case APP_CMD_LOST_FOCUS:
+{
+// Command from main thread: the app's activity window has lost
+// input focus.
+// WindowHandle defaultWindow = { 0 };
+// m_eventQueue.postSuspendEvent(defaultWindow, Suspend::WillSuspend);
+break;
+}
+
+case APP_CMD_CONFIG_CHANGED:
+// Command from main thread: the current device configuration has changed.
+break;
+
+case APP_CMD_LOW_MEMORY:
+// Command from main thread: the system is running low on memory.
+// Try to reduce your memory use.
+break;
+
+case APP_CMD_START:
+// Command from main thread: the app's activity has been started.
+break;
+
+case APP_CMD_RESUME:
+{
+	// Command from main thread: the app's activity has been resumed.
+	// WindowHandle defaultWindow = { 0 };
+	// m_eventQueue.postSuspendEvent(defaultWindow, Suspend::DidResume);
+	break;
+}
+
+case APP_CMD_SAVE_STATE:
+// Command from main thread: the app should generate a new saved state
+// for itself, to restore from later if needed.  If you have saved state,
+// allocate it with malloc and place it in android_app.savedState with
+// the size in android_app.savedStateSize.  The will be freed for you
+// later.
+break;
+
+case APP_CMD_PAUSE:
+{
+	// Command from main thread: the app's activity has been paused.
+	// WindowHandle defaultWindow = { 0 };
+	// m_eventQueue.postSuspendEvent(defaultWindow, Suspend::DidSuspend);
+	break;
+}
+
+case APP_CMD_STOP:
+// Command from main thread: the app's activity has been stopped.
+break;
+
+case APP_CMD_DESTROY:
+// Command from main thread: the app's activity is being destroyed,
+// and waiting for the app thread to clean up and exit before proceeding.
+// m_eventQueue.postExitEvent();
+break;
+}
+}
+
+
+void android_main(android_app* state)
+{
+	app_dummy();
+
+	struct engine engine;
+
+	memset(&engine, 0, sizeof(engine));
+	state->userData = &engine;
+	state->onAppCmd = handle_cmd;
+	state->onInputEvent = handle_input;
+	engine.app = state;
+
+	// Read all pending events.
+	while (1) {
+		int ident;
+		int events;
+		struct android_poll_source* source;
+
+		while ((ident=ALooper_pollAll(0, NULL, &events,(void**)&source)) >= 0) {
+
+			// Process this event.
+			if (source != NULL) {
+				source->process(state, source);
+			}
+
+			// Check if we are exiting.
+			if (state->destroyRequested != 0) {
+				terminate_display(&engine);
+				return;
+			}
+		}
+
+		// Draw the current frame
+		draw_frame(&engine);
+	}
+}
+*/
