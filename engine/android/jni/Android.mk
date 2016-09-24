0085a054ebd8d648e8b7f2b31f74f38e56336432
Colin <colingilbert86@gmail.com>
2016-09-24 16:08:54 -0600

    Mostly done refining Android buildscripts :)

--- engine/android/jni/Android.mk
+++ engine/android/jni/Android.mk
@@ -2,5 +2,4 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := native-activity
 LOCAL_SRC_FILES := libnative-activity.so
-#LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
 include $(PREBUILT_SHARED_LIBRARY)
