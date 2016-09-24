33bf9adf30303d686f854c9e2101093a390f6ab6
Colin <colingilbert86@gmail.com>
2016-09-24 15:34:26 -0600

    Further refinements

--- engine/android/jni/Android.mk
+++ engine/android/jni/Android.mk
@@ -1,5 +1,6 @@
- include $(CLEAR_VARS)
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
 LOCAL_MODULE := native-activity
-LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libnative-activity.so
+LOCAL_SRC_FILES := libnative-activity.so
 #LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
 include $(PREBUILT_SHARED_LIBRARY)
