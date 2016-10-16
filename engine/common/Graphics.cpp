8f89bed8ea21f93e2e38d0c98bc0af3e0671b847
Colin <colingilbert86@gmail.com>
2016-10-16 10:51:27 -0600

    Add GL program load function

--- engine/common/Graphics.cpp
+++ engine/common/Graphics.cpp
@@ -46,7 +46,7 @@ GLuint load_shader_gl(GLenum type, const std::string& shader_arg)
 			info_log.resize(info_len);
 
 			glGetShaderInfoLog(shader, info_len, NULL, &info_log[0]);
-			
+
 			noob::logger::log(noob::importance::ERROR, noob::concat("[Graphics] Error compiling shader: ", info_log));
 		}
 
@@ -55,6 +55,75 @@ GLuint load_shader_gl(GLenum type, const std::string& shader_arg)
 	}
 
 	return shader;
+}
+
+GLuint load_program_gl(const std::string& vert_shader_arg, const std::string frag_shader_arg)
+{
+	GLuint vertex_shader;
+	GLuint fragment_shader;
+	GLuint program_object;
+	GLint linked;
+
+	const char* vert_shader_src = vert_shader_arg.c_str();
+	// Load the vertex/fragment shaders
+	vertex_shader = load_shader_gl(GL_VERTEX_SHADER, vert_shader_arg);
+
+	if (vertex_shader == 0)
+	{
+		return 0;
+	}
+
+	const char* frag_shader_src  = frag_shader_arg.c_str();
+	fragment_shader = load_shader_gl(GL_FRAGMENT_SHADER, frag_shader_arg);
+
+	if (fragment_shader == 0)
+	{
+		glDeleteShader(vertex_shader);
+		return 0;
+	}
+
+	// Create the program object
+	program_object = glCreateProgram();
+
+	if (program_object == 0)
+	{
+		return 0;
+	}
+
+	glAttachShader(program_object, vertex_shader);
+	glAttachShader(program_object, fragment_shader);
+
+	// Link the program
+	glLinkProgram(program_object);
+
+	// Check the link status
+	glGetProgramiv(program_object, GL_LINK_STATUS, &linked);
+
+	if (!linked)
+	{
+		GLint info_len = 0;
+
+		glGetProgramiv(program_object, GL_INFO_LOG_LENGTH, &info_len);
+
+		if (info_len > 1)
+		{
+			std::string info_log;
+			info_log.resize(info_len);
+
+			glGetProgramInfoLog(program_object, info_len, NULL, &info_log[0]);
+			noob::logger::log(noob::importance::INFO, noob::concat("[Graphics] Error linking program:", info_log));
+
+		}
+
+		glDeleteProgram(program_object);
+		return 0;
+	}
+
+	// Free up no longer needed shader resources
+	glDeleteShader(vertex_shader);
+	glDeleteShader(fragment_shader);
+
+	return program_object;
 }
 
 
