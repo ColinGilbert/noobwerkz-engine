241cb9e449431f4e5b79fb0df8857deabf9b38a2
Colin <colingilbert86@gmail.com>
2016-12-08 15:12:49 -0600

    Cleanup

--- engine/common/LightingUniforms.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-namespace noob
-{
-	struct lighting_buffer
-	{
-		noob::vec4 eye_pos, directional;
-	};
-}
--- engine/common/UniformBlock.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-namespace noob
-{
-	struct uniform_block
-	{
-		uint32_t block_id, buffer_id, block_size, binding_point;
-	};
-}
